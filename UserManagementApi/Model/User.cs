475f84e60535ec6129e9b82e26865c75494bd518
VictorOkereke-dev <vicdocsbackup080@gmail.com>
2025-08-17 02:09:23 +0100

    Add files via upload

---
+++ UserManagementApi/Model/User.cs
@@ -0,0 +1,12 @@
+namespace UserManagementAPI.Models;
+
+public class User
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public int? Age { get; set; }
+}
